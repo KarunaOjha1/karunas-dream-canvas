0214b4e13411e24bff5380b621e62e6fd31868b7
KarunaOjha1 <ojhakaruna296@gmail.com>
2026-06-04 13:01:40 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "@lovable-dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: '/karunas-dream-canvas/',
   tanstackStart: {
     server: { entry: "server" },
+  },
+  vite: {
+    base: '/karunas-dream-canvas/',
   },
 });
